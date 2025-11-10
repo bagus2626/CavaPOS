646e6567fc6eb2f5781e5e9b1829744bed9470cc
farroshayray <farros.hr@gmail.com>
2025-11-10 08:54:34 +0700

    commit before merge

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -127,6 +127,17 @@
       0 => 'Laravel\\Sail\\SailServiceProvider',
     ),
   ),
+  'laravel/socialite' => 
+  array (
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+  ),
   'laravel/tinker' => 
   array (
     'providers' => 
@@ -194,13 +205,6 @@
     'providers' => 
     array (
       0 => 'Pest\\Laravel\\PestServiceProvider',
-    ),
-  ),
-  'phiki/phiki' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Phiki\\Adapters\\Laravel\\PhikiServiceProvider',
     ),
   ),
   'ryangjchandler/blade-capture-directive' => 
